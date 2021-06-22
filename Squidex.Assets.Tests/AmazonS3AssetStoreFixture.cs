e2733a23a1f28e8c361d69a4cf9f0f5fa849b44c
Sebastian <sebastian@squidex.io>
2021-06-22 18:04:45 +0200

    Remove key.

--- Squidex.Assets.Tests/AmazonS3AssetStoreFixture.cs
+++ Squidex.Assets.Tests/AmazonS3AssetStoreFixture.cs
@@ -16,12 +16,12 @@ namespace Squidex.Assets
             // From: https://console.aws.amazon.com/iam/home?region=eu-central-1#/users/s3?section=security_credentials
             AssetStore = new AmazonS3AssetStore(new AmazonS3Options
             {
-                AccessKey = "AKIAYR4IRKRWOZHXU5TB",
+                AccessKey = "key",
                 Bucket = "squidex-test",
                 BucketFolder = "squidex-assets",
                 ForcePathStyle = false,
                 RegionName = "eu-central-1",
-                SecretKey = "TbChpTIbBjTJfB6R/BNepIBF6S5g/vZedeH3057s",
+                SecretKey = "secret",
                 ServiceUrl = null
             });
             AssetStore.InitializeAsync().Wait();
