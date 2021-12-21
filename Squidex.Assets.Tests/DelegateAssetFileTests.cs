9ad489633b072966295052cd4e92f425ced56f00
Sebastian <sebastian@squidex.io>
2021-12-21 18:05:02 +0100

    Another test.

---
+++ Squidex.Assets.Tests/DelegateAssetFileTests.cs
@@ -0,0 +1,26 @@
+﻿// ==========================================================================
+//  Squidex Headless CMS
+// ==========================================================================
+//  Copyright (c) Squidex UG (haftungsbeschraenkt)
+//  All rights reserved. Licensed under the MIT license.
+// ==========================================================================
+
+using System.IO;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Squidex.Assets
+{
+    public class DelegateAssetFileTests
+    {
+        [Fact]
+        public void Should_be_serializable_to_json()
+        {
+            var source = new DelegateAssetFile("fileName", "file/type", 1024, () => new MemoryStream());
+
+            var deserialized = JsonConvert.DeserializeObject<DelegateAssetFile>(JsonConvert.SerializeObject(source));
+
+            Assert.Equal(source.FileName, deserialized.FileName);
+        }
+    }
+}
